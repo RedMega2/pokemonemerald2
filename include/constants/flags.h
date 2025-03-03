d3d3f2251036beb123d682b8be865cab907419e0
Zeroth Veratross <120138654+ZerothVeratross@users.noreply.github.com>
2025-03-03 16:30:18 -0300

    Route 106 South scripting and overworld pokemon in southwest areas
    Pokemon are included in Route 105, 106, 106 South and Dewford Town

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1542,7 +1542,7 @@
 #define FLAG_HIDE_JIRACHI                           (SYSTEM_FLAGS + 0x9F)
 #define FLAG_DEFEATED_JIRACHI                       (SYSTEM_FLAGS + 0xA0)
 #define FLAG_ROUTE_116_HIDE_KIRLIA                  (SYSTEM_FLAGS + 0xA1)
-#define FLAG_UNUSED_0x902                           (SYSTEM_FLAGS + 0xA2) // Unused Flag
+#define FLAG_UNUSED_0x902							(SYSTEM_FLAGS + 0xA2) // Unused Flag
 #define FLAG_UNUSED_0x903                           (SYSTEM_FLAGS + 0xA3) // Unused Flag
 #define FLAG_UNUSED_0x904                           (SYSTEM_FLAGS + 0xA4) // Unused Flag
 #define FLAG_UNUSED_0x905                           (SYSTEM_FLAGS + 0xA5) // Unused Flag
